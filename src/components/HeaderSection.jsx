cbd03ff8c46c32bdafc304805ef08729864b936d
Atanu Kumar Dey <atanukumardey_2020@gift.edu.in>
2023-05-30 11:23:57 +0530

    done

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -3,19 +3,49 @@ import Box from "@mui/material/Box";
 import { FaUserCircle } from "react-icons/fa";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { useSelector } from "react-redux";
+import { Alert, Collapse, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import VerifiedIcon from "@mui/icons-material/Verified";
+import { AlertTitle } from "@mui/material";
+
 
 const HeaderSection = () => {
+  const [open, setOpen] = React.useState(true);
   const { value } = useSelector((state) => state.step);
   return (
     <>
-      {value==3?(<Box display="flex" sx={{ p: 2, width: "100%" }}>
-          <Box sx={{ width: "70%", fontWeight: "700", fontSize: "28px" }}>
+      {value == 3 ? (
+        <Box display="flex"  sx={{width: "100%",justifyContent:"space-between" }}>
+          <Box sx={{p:2, fontWeight: "700", fontSize: "28px" }}>
             Test_Study
           </Box>
-          
+          <div style={{position:"fixed",zIndex: 199,right:"30%"}}>
+            <Collapse in={open}>
+              <Alert
+                severity="success"
+                sx={{backgroundColor:"rgba(241, 255, 247, 1)"}}
+                icon={<VerifiedIcon />}
+                action={
+                  <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                      setOpen(false);
+                    }}>
+                    <CloseIcon fontSize="inherit" />
+                  </IconButton>
+                }
+                >
+                <AlertTitle sx={{color:"rgba(16, 164, 75, 1)",fontSize:"16px"}}>Success</AlertTitle>
+                <span style={{fontSize:"14px"}}> Channels configured successfully. </span>
+              </Alert>
+            </Collapse>
+          </div>
+
           <Box
             sx={{
-              width: "50%",
+              p:2,
               display: "flex",
               justifyContent: "right",
               alignItems: "center",
@@ -24,7 +54,8 @@ const HeaderSection = () => {
             &nbsp;Atanu Kumar Dey
             <RiArrowDropDownLine size={20} />
           </Box>
-        </Box>): (
+        </Box>
+      ) : (
         <Box display="flex" sx={{ p: 2, width: "100%" }}>
           <Box sx={{ width: "70%", fontWeight: "700", fontSize: "28px" }}>
             Test_Study
--- src/components/Rows.jsx
+++ src/components/Rows.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { decrement } from "../store/stateSlice";
 import { updateDropdownData } from "../store/dropdownSlice";
@@ -10,27 +10,23 @@ import SubRows from "./SubRows";
 import DropDown from "./DropDown";
 
 const Rows = ({ rowId, channel }) => {
- 
   const dropdownData = useSelector((state) => state.dropdown);
- 
-  console.log(dropdownData[rowId])
   const { value } = useSelector((state) => state.step);
   const dispatch = useDispatch();
+  const [clickedOnce, setClickedOnce] = useState(false);
   const [open, setOpen] = useState(false);
   const [showAdditionalComponent, setShowAdditionalComponent] = useState(false);
   const [clickCount, setclickCount] = useState(1);
 
   const handleButtonClick = (subrowId) => {
     setclickCount(clickCount + 1);
-    dispatch(updateDropdownData({ rowId,subrowId }));
+    dispatch(updateDropdownData({ rowId, subrowId }));
   };
 
   const handleExpand = (subrowId) => {
     setShowAdditionalComponent(!showAdditionalComponent);
-    dispatch(updateDropdownData({ rowId,subrowId }));
-
-   
-
+    dispatch(updateDropdownData({ rowId, subrowId }));
+    setClickedOnce(true);
   };
   const handleClose = () => {
     setOpen(false);
@@ -49,104 +45,122 @@ const Rows = ({ rowId, channel }) => {
     dispatch(updateDropdownData({ rowId, dropdownId, data }));
   };
 
-
   return (
-  dropdownData[rowId]&&   <div>
-  <Box sx={{ mb: 3 }}>
-    <Box
-      sx={{
-        width: "100%",
-        minHeight: "80px",
-        backgroundColor: "white",
-        display: "flex",
-        boxShadow: 2,
-        alignItems: "center",
-      }}
-      key={rowId}>
-      <Box
-        sx={{
-          width: "20%",
-          display: "flex",
-          justifyContent: "center",
-          fontWeight: "500",
-        }}>
-        {channel}
-      </Box>
-      <Box sx={{ width: "30%", display: "flex", justifyContent: "center" }}>
-    
-          <DropDown
-          value={dropdownData[rowId][0].dropdown1} // Assuming dropdown1 is the ID of your first dropdown
-          onChange={(data) => handleDropdownChange("dropdown1", data)}
-        />
-          
-      
-        
-      </Box>
-      <Box sx={{ width: "30%", display: "flex", justifyContent: "center" }}>
-        <DropDown
-          value={dropdownData[rowId][0].dropdown2} // Assuming dropdown2 is the ID of your second dropdown
-          onChange={(data) => handleDropdownChange("dropdown2", data)}
-        />
-      </Box>
-      <Box sx={{ width: "20%", display: "flex", justifyContent: "center" }}>
-        {value < 2 ? (
-          <Button
-            size="medium"
-            sx={{ fontSize: "12px", textTransform: "capitalize" }}
-            startIcon={<AiOutlinePlus />}
-            onClick={()=>handleExpand(clickCount)}>
-            {showAdditionalComponent
-              ? "Hide Backup Channel"
-              : "Add Backup Channel"}
-          </Button>
-        ) : (
-          <Button
-            size="medium"
-            variant="outlined"
-            disabled={value > 2}
-            sx={{ fontSize: "12px", textTransform: "capitalize " }}
-            startIcon={<EditIcon />}
-            onClick={handleOpen}>
-            Edit backup channel
-          </Button>
-        )}
-      </Box>
-    </Box>
-    {showAdditionalComponent && (
-      <Box
-        sx={{
-          boxShadow: 2,
-          py:2,
-          overflow: "hidden",
-          backgroundColor: "rgba(246, 246, 246, 1)",
-        }}>
-       {Array.from({ length: clickCount }).map((_, index) => (
-          <SubRows key={index} rowId={rowId} subrowId={index+1} />
-        ))}
-        <Box sx={{ width: "60%", textAlign: "center" }}>
-          <Button
-            size="medium"
-            sx={{ fontSize: "12px", textTransform: "capitalize " }}
-            startIcon={<AiOutlinePlus />}
-            onClick={()=>handleButtonClick(clickCount+1)}>
-            Add Backup Channel
-          </Button>
+    dropdownData[rowId] && (
+      <div>
+        <Box sx={{ mb: 3 }}>
+          <Box
+            sx={{
+              width: "100%",
+              minHeight: "80px",
+              backgroundColor: "white",
+              display: "flex",
+              boxShadow: 2,
+              alignItems: "center",
+            }}
+            key={rowId}>
+            <Box
+              sx={{
+                width: "20%",
+                display: "flex",
+                justifyContent: "center",
+                fontWeight: "500",
+              }}>
+              {channel}
+            </Box>
+            <Box
+              sx={{ width: "30%", display: "flex", justifyContent: "center" }}>
+              <DropDown
+                value={dropdownData[rowId][0].dropdown1} // Assuming dropdown1 is the ID of your first dropdown
+                onChange={(data) => handleDropdownChange("dropdown1", data)}
+                rowId={rowId}
+                dropdownId={"dropdown1"}
+              />
+            </Box>
+            <Box
+              sx={{ width: "30%", display: "flex", justifyContent: "center" }}>
+              <DropDown
+                value={dropdownData[rowId][0].dropdown2} // Assuming dropdown2 is the ID of your second dropdown
+                onChange={(data) => handleDropdownChange("dropdown2", data)}
+                rowId={rowId}
+                dropdownId={"dropdown2"}
+              />
+            </Box>
+            <Box
+              sx={{ width: "20%", display: "flex", justifyContent: "center" }}>
+              {value < 2 ? (
+                <Button
+                  size="medium"
+                  sx={{ fontSize: "12px", textTransform: "capitalize" }}
+                  onClick={() => handleExpand(clickCount)}>
+                  {showAdditionalComponent ? (
+                    "Hide Backup Channel"
+                  ) : (
+                    <>
+                      {clickCount > 1 ? (
+                        `View backup channels(${clickCount})`
+                      ) : clickedOnce ? (
+                        `View backup channels(${clickCount})`
+                      ) : (
+                        <Button
+                          size="medium"
+                          sx={{ fontSize: "12px", textTransform: "capitalize" }}
+                          startIcon={<AiOutlinePlus />}>
+                          Add Backup Channel
+                        </Button>
+                      )}
+                    </>
+                  )}
+                </Button>
+              ) : (
+                <Button
+                  size="medium"
+                  variant="outlined"
+                  disabled={value > 2}
+                  sx={{ fontSize: "12px", textTransform: "capitalize " }}
+                  startIcon={<EditIcon />}
+                  onClick={handleOpen}>
+                  Edit Channel
+                </Button>
+              )}
+            </Box>
+          </Box>
+          {showAdditionalComponent && (
+            <Box
+              sx={{
+                boxShadow: 2,
+                py: 2,
+                overflow: "hidden",
+                backgroundColor: "rgba(246, 246, 246, 1)",
+              }}>
+              {Array.from({ length: clickCount }).map((_, index) => (
+                <SubRows key={index} rowId={rowId} subrowId={index + 1} />
+              ))}
+              <Box sx={{ width: "60%", textAlign: "center" }}>
+                <Button
+                  size="medium"
+                  sx={{ fontSize: "12px", textTransform: "capitalize " }}
+                  startIcon={<AiOutlinePlus />}
+                  onClick={() => handleButtonClick(clickCount + 1)}>
+                  Add Backup Channel
+                </Button>
+              </Box>
+            </Box>
+          )}
         </Box>
-      </Box>
-    )}
-  </Box>
-  <Backdrop
-    sx={{
-      color: "#fff",
-      zIndex: (theme) => theme.zIndex.drawer + 1,
-      display: "flex",
-      flexDirection: "column",
-    }}
-    open={open}>
-    <CircularProgress color="inherit" sx={{ mb: 2 }} />
-    {value === 3 ? <p>Saving All Changes...</p> : null}
-  </Backdrop>
-</div>
+        <Backdrop
+          sx={{
+            color: "#fff",
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+            display: "flex",
+            flexDirection: "column",
+          }}
+          open={open}>
+          <CircularProgress color="inherit" sx={{ mb: 2 }} />
+          {value === 3 ? <p>Saving All Changes...</p> : null}
+        </Backdrop>
+      </div>
+    )
   );
 };
 
